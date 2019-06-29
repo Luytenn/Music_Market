d1ef51025dc9a917f29f0c710219c50f96ff0167
Juan <amvcancioness@hotmail.com>
2019-06-29 19:46:31 -0400

    Creacion de dos clases

---
+++ src/model/test.java
@@ -0,0 +1,34 @@
+package model;
+
+public class test {
+
+	int color,direcc,cant;
+
+	public int getColor() {
+		return color;
+	}
+
+	public void setColor(int color) {
+		this.color = color;
+	}
+
+	public int getDirecc() {
+		return direcc;
+	}
+
+	public void setDirecc(int direcc) {
+		this.direcc = direcc;
+	}
+
+	public int getCant() {
+		return cant;
+	}
+
+	public void setCant(int cant) {
+		this.cant = cant;
+	}
+	
+	
+	
+	
+}
